38691b768a42fcc541ecd402d1919dba05cdf23f
ssbarbee <ssbarbee@users.noreply.github.com>
2023-11-18 23:50:24 +0100

    feat: add Enter keypress

--- src/scenes/DungeonScene.ts
+++ src/scenes/DungeonScene.ts
@@ -1,6 +1,6 @@
 import { Container } from 'pixi.js';
 
-import { Dungeon } from '../generators/map/Dungeon/Dungeon';
+import { generateDungeon } from '../generators/map/Dungeon/Dungeon';
 import { DungeonRenderer } from '../generators/map/Dungeon/DungeonRenderer';
 import { IScene } from '../Manager';
 
@@ -8,9 +8,25 @@ export class DungeonScene extends Container implements IScene {
   constructor() {
     super();
 
-    const dungeon = new Dungeon(10);
+    this.generateAndDrawDungeon();
+
+    window.addEventListener('keypress', (e) => this.handleKeypress(e));
+  }
+
+  private generateAndDrawDungeon(): void {
+    // Clear the container of old dungeon elements
+    this.removeChildren();
+
+    // Generate and draw the new dungeon
+    const dungeon = generateDungeon(10);
     const dungeonRenderer = new DungeonRenderer(dungeon, this);
     dungeonRenderer.draw();
+  }
+
+  private handleKeypress(e: KeyboardEvent): void {
+    if (e.key === 'Enter') {
+      this.generateAndDrawDungeon();
+    }
   }
 
   public update(): void {}
